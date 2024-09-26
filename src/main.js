07d5b7d51d6dfaf3faf8c2e1f1141441846017e5
thillo <salysem90@gmail.com>
2024-09-27 00:17:25 +0200

    i18n for translate initialezed

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.vue'
 import router from './router'
+import  fr  from './lang/fr.json'
+import  en  from './lang/en.json'
 const app = createApp(App)
 
+const i18n = createI18n({
+    lang: 'en', // Langue par défaut
+    fallbacklang: 'en', // Langue de secours
+    messages: {
+      en,
+      fr
+    }
+  });
+
+app.use(createI18n())
 app.use(createPinia())
 app.use(router)
 
