e8b404ca585bf8056396e3f225cfa66a4c13fd2e
thillo <salysem90@gmail.com>
2024-09-28 15:05:12 +0200

    translation pages modified contune

---
+++ src/Lang.vue
@@ -0,0 +1,25 @@
+<template>
+    <select class="form-select" aria-label="Default select example" @change="changeLanguage($event)">
+      <option value="en">EN</option>
+      <option value="fr">FR</option>
+    </select>
+  </template>
+  
+  <script setup>
+  import { getCurrentInstance } from 'vue';
+  const { proxy } = getCurrentInstance();
+  
+  const changeLanguage = (event) => {
+    const selectedLanguage = event.target.value;
+    proxy.$i18n.locale = selectedLanguage;
+  };
+  </script>
+  
+  <style scoped>
+  .form-select {
+    width: 80px; 
+    color: rgb(8, 14, 26);
+    padding: 5px; 
+  }
+  </style>
+  
